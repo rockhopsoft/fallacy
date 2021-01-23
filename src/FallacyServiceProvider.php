9f0442b14221d28bdc7428d1d5ec52cd341a568b
Morgan Lesko <15270653+rockhopsoft@users.noreply.github.com>
2021-01-23 11:38:30 -0500

    Update FallacyServiceProvider.php

--- src/FallacyServiceProvider.php
+++ src/FallacyServiceProvider.php
@@ -5,17 +5,26 @@ use Illuminate\Support\ServiceProvider;
 
 class FallacyServiceProvider extends ServiceProvider
 {
+    /**
+     * All of the container bindings that should be registered.
+     *
+     * @var array
+     */
+    public $bindings = [
+        ServerProvider::class => FallacyServiceProvider::class,
+    ];
+    
     public function boot()
     {
         require __DIR__ . '/routes.php';
         $this->publishes([
               __DIR__.'/Views'   => base_path('resources/views/vendor/fallacy'),
-              __DIR__.'/Models'  => base_path('app/Models/Fallacy'),
               __DIR__.'/Models'  => base_path('app/Models'),
               __DIR__.'/Uploads' => base_path('storage/app/up/fallacy'),
               __DIR__.'/Database/2019_11_16_000000_create_fallacy_tables.php'
                   => base_path('database/migrations/2019_11_16_000000_create_fallacy_tables.php'),
-              __DIR__.'/Database/FallacySeeder.php' => base_path('database/seeders/FallacySeeder.php')
+              __DIR__.'/Database/FallacySeeder.php' 
+                  => base_path('database/seeders/FallacySeeder.php')
         ]);
     }
 }
